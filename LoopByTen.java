893c255cbb002d9e9d106edc365aad22bf902818
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-10-29 16:50:17 -0700

    Create LoopByTen.java

---
+++ LoopByTen.java
@@ -0,0 +1,17 @@
+// File: LoopByTen.java
+// Programmer: Jennifer King
+// Date: 10/29/2018
+
+public class LoopByTen 
+{
+	public static void main(String[] args)
+	{
+		int x = 0;
+		for(; x < 1000; x+=10)
+		{
+			System.out.println(x+",");
+		}
+		System.out.println(x);
+	}
+
+}
